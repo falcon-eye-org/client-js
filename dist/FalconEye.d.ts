fc88bf7e495419e0748147f2d201c4871b7c009c
Julien Tauran <julien.tauran@epitech.eu>
2019-07-10 17:13:20 +0200

    Removed transpiled files

--- dist/FalconEye.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class FalconEye {
-    private apiKey;
-    private hasStarted;
-    private eventListener;
-    private config;
-    constructor(apiKey: string);
-    observe(): void;
-    getAPIKey(): string;
-}

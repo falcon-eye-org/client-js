db7328dbb3cb04e3a0301a0917b3e613bca1ee1c
Julien Tauran <julien.tauran@epitech.eu>
2019-07-12 00:22:53 +0200

    Added a handshake to the server

---
+++ src/network/Network.ts
@@ -0,0 +1,38 @@
+import { FalconEye } from "../FalconEye";
+import * as request from "request-promise";
+
+export class Network {
+
+    private falconEye: FalconEye;
+    private address: string;
+    private profileId: string;
+
+    constructor(falconEye: FalconEye, address: string) {
+        this.falconEye = falconEye;
+        this.address = address;
+        this.profileId = "";
+    }
+
+    public async handshake(): Promise<void> {
+        let This = this;
+
+        await request({
+            method: 'POST',
+            uri: this.address + (this.address.slice(-1) === '/' ? "" : "/") +  "connect/",
+            body: {
+                apikey: this.falconEye.getAPIKey()
+            },
+            json: true
+        })
+        .then((parsedBody: any) => {
+            This.profileId = parsedBody.profileid;
+        })
+        .catch((err: any) => {
+            console.error("[FalconEye] Failed to handshake the server ! " + err);
+        });
+    }
+
+    public getProfileId(): string {
+        return this.profileId;
+    }
+}

0fa0aa025a4e709ee8e07527fbf8eae824531344
Julien Tauran <julien.tauran@epitech.eu>
2019-07-12 22:48:04 +0200

    Added a storage class

---
+++ src/storage/Storage.ts
@@ -0,0 +1,15 @@
+import { FalconEye } from "../FalconEye";
+import { FalconEvent } from "../event/FalconEvent";
+
+export class Storage {
+    private falconEye: FalconEye;
+
+    constructor(falconEye: FalconEye) {
+        this.falconEye = falconEye;
+    }
+
+    public storeEvent<T extends FalconEvent>(event: T) {
+        // if (typeof event == )
+        console.log(typeof event);
+    }
+}
